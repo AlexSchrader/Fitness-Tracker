c029e4445dad53072be64d898334f3db5b884d1d
Alex Schrader <aschrader@my.waketech.edu>
2026-05-23 18:08:17 -0400

    fix icon paths

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,10 @@ export default defineConfig({
         theme_color: '#0a0a0a',
         background_color: '#0a0a0a',
         display: 'standalone',
-        icons: [{ src: '/icon.png', sizes: '192x192', type: 'image/png' }]
+        icons: [
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
       }
     })
   ]
